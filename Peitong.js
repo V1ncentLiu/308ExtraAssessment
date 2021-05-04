00ed5fa12bd314cfa8495c3cd8d8594d7117fe47
Peitong Liu <vincentliu9816@gmail.com>
2021-05-03 23:43:54 -0400

    Mode Switch Implemented, Ver. Beta 1.0.

--- Peitong.js
+++ Peitong.js
@@ -1,20 +1,22 @@
+//Variables
 let renderer, scene, camera, pointlight, spotlight, shaderMaterial;
+let mode = true;// game mode, true for player vs cpu, false for cpu vs cpu
 let fieldW = 400,
     fieldH = 200;
 let blockW, blockH, blockD, blockQ;
-let block1Y = 0,
-    block2Y = 0,
+let block1Y = 0,// velocity of player paddle on y axis
+    block2Y = 0,// velocity of opponent paddle on y aixs.
     blockV = 3;
 let ball, block1, block2;
 let ballX = 1,
     ballY = 1,
-    ballVMax = 3.5,
+    ballVMax = 3.5,// maximum speed for the ball, 
     ballV = 1;
-let score1 = 0,
-    score2 = 0,
-    scoreMax = 15;
+let score1 = 0,// player score
+    score2 = 0,// opponent score
+    scoreMax = 15;//
 let diff = 0.5;
-let TABLE_TEXTURE_IMG = "floor-wood.jpg";
+let TABLE_TEXTURE_IMG = "floor-wood.jpg";//texture is loaded by threejs texture loader
 const clock = new THREE.Clock();
 const __shader = Shaders.S01;
 
@@ -34,12 +36,13 @@ function createScene() {
         NEAR = 1,
         FAR = 1000000;
     var c = document.getElementById("canvas");
+    //Settings of the vertex shader and the fragment shader is in shader.js.
     shaderMaterial = new THREE.ShaderMaterial(
         {
             uniforms: __shader.uniforms,
-            vertexShader:__shader.vertexShader,
-            fragmentShader:__shader.fragmentShader,
-            transparent:true,
+            vertexShader: __shader.vertexShader,
+            fragmentShader: __shader.fragmentShader,
+            transparent: true,
         }
     );
     renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -52,19 +55,21 @@ function createScene() {
     var planeW = fieldW,
         planeH = fieldH,
         planeQ = 10;
-    var block1Mat = new THREE.MeshLambertMaterial({ color: 0x66ccff });
+    var block1Mat = new THREE.MeshLambertMaterial({ color: 0x6600ff });
     var block2Mat = new THREE.MeshLambertMaterial({ color: 0xf00606 });
-    var planeMat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    var planeMat = new THREE.MeshLambertMaterial({ color: 0x777777 });
     var tableMat = new THREE.MeshLambertMaterial({ color: 0x111111 });
     var sidesMat = new THREE.MeshLambertMaterial({ color: 0x000000 });
     Texture = new THREE.TextureLoader().load(TABLE_TEXTURE_IMG);
     var groundMat = new THREE.MeshLambertMaterial({ map: Texture });
+    //Create surface
     var plane = new THREE.Mesh(
         new THREE.PlaneGeometry(planeW * 0.95, planeH, planeQ, planeQ),
         planeMat
     );
     scene.add(plane);
     plane.receiveShadow = true;
+    //Create background
     var table = new THREE.Mesh(
         new THREE.BoxGeometry(planeW * 1.05, planeH * 1.05, 100, planeQ, planeQ, 1),
         tableMat
@@ -72,12 +77,13 @@ function createScene() {
     table.position.z = -51;
     scene.add(table);
     table.receiveShadow = true;
+    //Create the ball
     var ballRad = 6,
         ballSeg = 6,
         ballRings = 6;
-    
-    __shader.uniforms.textureA.value = new THREE.TextureLoader().load('stone-bump.jpg');
-    __shader.uniforms.textureB.value = new THREE.TextureLoader().load('floor-wood.jpg')
+    //Both of the shader material are retrived from classwork.
+    __shader.uniforms.textureA.value = new THREE.TextureLoader().load('stone-bump.jpg');// Vertex shader material
+    __shader.uniforms.textureB.value = new THREE.TextureLoader().load('floor-wood.jpg');// Fragment shader material
     ball = new THREE.Mesh(
         new THREE.SphereBufferGeometry(ballRad, ballSeg, ballRings),
         shaderMaterial
@@ -93,6 +99,7 @@ function createScene() {
     blockH = 32;
     blockD = 10;
     blockQ = 2;
+    //Block 1 is the paddle for the player.
     block1 = new THREE.Mesh(
         new THREE.BoxGeometry(blockW, blockH, blockD, blockQ, blockQ, blockQ),
         block1Mat
@@ -100,6 +107,7 @@ function createScene() {
     scene.add(block1);
     block1.receiveShadow = true;
     block1.castShadow = true;
+    //Block 2 is the paddle for the opponent.
     block2 = new THREE.Mesh(
         new THREE.BoxGeometry(blockW, blockH, blockD, blockQ, blockQ, blockQ),
         block2Mat
@@ -111,6 +119,7 @@ function createScene() {
     block2.position.x = fieldW / 2 - blockW;
     block1.position.z = blockD;
     block2.position.z = blockD;
+    //Actual background.
     var ground = new THREE.Mesh(
         new THREE.BoxGeometry(2000, 1000, 3, 1, 1, 1),
         groundMat
@@ -118,6 +127,7 @@ function createScene() {
     ground.position.z = -150;
     ground.receiveShadow = true;
     scene.add(ground);
+    //Lights.
     pointlight = new THREE.PointLight(0xf8f8f8, 2.9, 10000);
     pointlight.position.x = -1000;
     pointlight.position.y = 0;
@@ -130,18 +140,9 @@ function createScene() {
     renderer.shadowMap.enabled = true;
 }
 
-function renderScene() {
-    requestAnimationFrame(renderScene);
-    __shader.uniforms.time.value = clock.getElapsedTime();
-    renderer.render(scene, camera);
-    ballPhys();
-    blockPhys();
-    cam();
-    playerPhys();
-    opponentPhys();
-}
-
+//Mechanics of the ball
 function ballPhys() {
+    //If the ball went over the bottom line of each side, add score accordingly and reset the ball.
     if (ball.position.x <= -fieldW / 2) {
         score2++;
         document.getElementById("score").innerHTML = score1 + "-" + score2;
@@ -154,6 +155,7 @@ function ballPhys() {
         resetBall(1);
         scoreCheck();
     }
+    //If ball went over the sides, bounce the ball back(Collision detection for the ball).
     if (ball.position.y <= -fieldH / 2) {
         ballY = -ballY;
     }
@@ -173,6 +175,7 @@ function ballPhys() {
         ballY = -ballV * 2;
     }
 }
+//simple mechanics for the opponent paddle
 function opponentPhys() {
     if (ballX > 0 && ball.position.x >= -fieldW / 3) {
         block2Y = (ball.position.y - block2.position.y) * diff;
@@ -185,17 +188,35 @@ function opponentPhys() {
                 block2.position.y -= blockV;
             }
         }
-        // block2.scale.y += (1 - block2.scale.y) * 0.2;
     }
 }
+//player side cpu mechanics, identical to the opponent side.
+function playerSideCPUPhys() {
+    if (ballX < 0 && ball.position.x <= fieldW / 3) {
+        block1Y = (ball.position.y - block1.position.y) * diff;
+        if (Math.abs(block1Y) <= blockV) {
+            block1.position.y += block1Y;
+        } else {
+            if (block1Y > blockV) {
+                block1.position.y += blockV;
+            } else if (block1Y < -blockV) {
+                block1.position.y -= blockV;
+            }
+        }
+    }
+}
+//Controls('Controls.js') for the player
 function playerPhys() {
+    //left
     if (Key.isDown(Key.A)) {
         if (block1.position.y < fieldH * 0.45) {
             block1Y = blockV * 0.5;
         } else {
             block1Y = 0;
         }
-    } else if (Key.isDown(Key.D)) {
+    }
+    //right
+    else if (Key.isDown(Key.D)) {
         if (block1.position.y > -fieldH * 0.45) {
             block1Y = -blockV * 0.5;
         } else {
@@ -206,7 +227,7 @@ function playerPhys() {
     }
     block1.position.y += block1Y;
 }
-
+//Put the camera 'behind' the player.
 function cam() {
     camera.position.x = block1.position.x - 100;
     camera.position.y = (block1.position.y - camera.position.y) * 0.5;
@@ -214,6 +235,7 @@ function cam() {
     camera.rotation.y = (-60 * Math.PI) / 180;
     camera.rotation.z = (-90 * Math.PI) / 180;
 }
+//Collision detection for the paddles.
 function blockPhys() {
     //////////////////////////player////////////////////////
     if (
@@ -246,7 +268,8 @@ function blockPhys() {
         }
     }
 }
-
+//Function for resetting the ball, parameter is the condition.
+//1 is if player wins, any other number is for opponent wins.
 function resetBall(num) {
     ball.position.x = 0;
     ball.position.y = 0;
@@ -255,11 +278,12 @@ function resetBall(num) {
     } else {
         ballX = 1;
     }
+    //also reset the ball's speed and Y position.
     ballY = 1;
     ballV = 0.5;
 }
 var bounceT = 0;
-
+//Check the score after each round, change the score board accordingly.
 function scoreCheck() {
     if (score1 >= scoreMax) {
         ballV = 0;
@@ -275,8 +299,33 @@ function scoreCheck() {
         document.getElementById("winCondition").innerHTML = "Please F5 to replay.";
         bounceT++;
         block2.position.z = Math.sin(bounceT * 0.1) * 10;
-        // enlarge and squish paddle to emulate joy
         block2.scale.z = 2 + Math.abs(Math.sin(bounceT * 0.1)) * 10;
         block2.scale.y = 2 + Math.abs(Math.sin(bounceT * 0.1)) * 10;
+    }
+}
+//Game mode switching function
+function switchMode() {
+    if (mode == true) {
+        mode = false;
+    } else if (mode == false) {
+        mode = true;
+    }
+}
+//Main render function
+function renderScene() {
+    requestAnimationFrame(renderScene);
+    __shader.uniforms.time.value = clock.getElapsedTime();
+    renderer.render(scene, camera);
+    ballPhys();
+    blockPhys();
+    cam();
+    opponentPhys();
+    //Switching mode is available during the game.
+    if (mode) {
+        playerPhys();
+        document.getElementById("currMode").innerHTML = "Current Mode: P1 vs CPU";
+    } else if (!mode) {
+        playerSideCPUPhys();
+        document.getElementById("currMode").innerHTML = "Current Mode: CPU vs CPU";
     }
 }
